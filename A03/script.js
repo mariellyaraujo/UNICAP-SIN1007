ea7a0f158db08f1e118d1c811a88f503c783ede0
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-11 13:49:33 -0300

    definindo as constantes no script

---
+++ A03/script.js
@@ -0,0 +1,6 @@
+function armazenarDados(){
+    const nome = document.getElementById("nomeForms").value;
+    const endereco = document.getElementById("enderecoForms").value;
+    const email = document.getElementById("emailForms").value;
+    const telefone = document.getElementById("telefoneForms").value;
+}
