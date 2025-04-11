fb3314e9cfffbe1133d08105de172a0dcbb71cef
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-04-11 13:52:13 -0300

    criação da função armazenarDados

--- A03/script.js
+++ A03/script.js
@@ -3,4 +3,11 @@ function armazenarDados(){
     const endereco = document.getElementById("enderecoForms").value;
     const email = document.getElementById("emailForms").value;
     const telefone = document.getElementById("telefoneForms").value;
+
+    localStorage.setItem("nomeForms", nome);
+    localStorage.setItem("enderecoForms", endereco);
+    localStorage.setItem("emailForms", email);
+    localStorage.setItem("telefoneForms", telefone);
+
+    alert("Dados registrados com sucesso!");
 }
